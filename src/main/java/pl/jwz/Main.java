109e37fc1116ea5f561c0f742325fc59002ff51a
Mariusz Wojdalski <mariuszwojdalski123@gmail.com>
2023-12-13 22:22:59 +0100

    okienko start / stop

--- src/main/java/pl/jwz/Main.java
+++ src/main/java/pl/jwz/Main.java
@@ -1,7 +1,10 @@
 package pl.jwz;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        Window windowGame = new Window(new Game());
+        Start windowStart = new Start(new Game());
+
     }
 }
